35ec487ececa0bbf95155ab740f73ddca8264b1d
Xiaoxiang <wangxiaoxiang-cn@foxmail.com>
2016-10-12 17:36:53 +0800

    URL coder error

--- LTM/LTMURLCoder.m
+++ LTM/LTMURLCoder.m
@@ -8,64 +8,28 @@
 
 #import "LTMURLCoder.h"
 
-static BOOL g_allowedCharacters = NO;
-static BOOL g_stringByRemovingPercentEncoding = NO;
-
 
 @implementation LTMURLCoder
 
-+ (void)initialize
++ (NSString *)decode:(NSString *)value
 {
-    g_allowedCharacters = [NSString instancesRespondToSelector:@selector(stringByAddingPercentEncodingWithAllowedCharacters:)];
-    g_stringByRemovingPercentEncoding = [NSString instancesRespondToSelector:@selector(stringByRemovingPercentEncoding)];
-}
-
-+ (NSString *)encode:(NSString *)string {
-    // Encode all the reserved characters, per RFC 3986
-    // (<http://www.ietf.org/rfc/rfc3986.txt>)
-    
-    static NSString * const  allowedCharacters = @"!*'();:@&=+$,/?%#[]";
-    if (g_allowedCharacters) {
-        static NSCharacterSet * characterSet = nil;
-        if (characterSet == nil) {
-            characterSet = [NSCharacterSet characterSetWithCharactersInString:allowedCharacters];
-        }
-        return  [string stringByAddingPercentEncodingWithAllowedCharacters:characterSet];
-    } else {
-        CFStringRef escaped = NULL;
+    value = [value stringByReplacingOccurrencesOfString:@"+" withString:@" "];
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        escaped = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
-                                                          (CFStringRef)string,
-                                                          NULL,
-                                                          (CFStringRef)allowedCharacters,
-                                                          kCFStringEncodingUTF8);
+    value = [value stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
 #pragma clang diagnostic pop
-        
-#if defined(__has_feature) && __has_feature(objc_arc)
-        return CFBridgingRelease(escaped);
-#else
-        return [(NSString *)escaped autorelease];
-#endif
-    }
+    return value;
 }
 
-+ (NSString *)decode:(NSString *)string {
-    NSMutableString *resultString = [NSMutableString stringWithString:string];
-    [resultString replaceOccurrencesOfString:@"+"
-                                  withString:@" "
-                                     options:NSLiteralSearch
-                                       range:NSMakeRange(0, [resultString length])];
-    
-    
-    if (g_stringByRemovingPercentEncoding) {
-        return resultString.stringByRemovingPercentEncoding;
-    }else{
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        return [resultString stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-#pragma clang diagnostic pop
-    }
-    
++ (NSString *)encode:(NSString *)value
+{
+    return (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(NULL,
+                                                                                 (CFStringRef)value,
+                                                                                 NULL, // characters to leave unescaped
+                                                                                 CFSTR(":!*();@/&?+$,='"),
+                                                                                 kCFStringEncodingUTF8);
 }
+#pragma clang diagnostic pop
 @end
